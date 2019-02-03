4938efd9ecbbf29c1af30a1ea9f8e1196d358e53
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-02-03 04:07:04 +0000

    Navigation bar hamburger button toggle functionality added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dimitar on 03/02/2019.
+ */
+function toggleHamburgerButtonFunction() {
+    document.getElementById("collapse_target").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.navbar-toggler')) {
+        var dropdowns = document.getElementsByClassName("navbar-collapse");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
