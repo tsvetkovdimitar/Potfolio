c38ba33152bb20cf490f830f6dadf2bf416097fe
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-02-06 18:53:17 +0000

    Projects section with styling added

--- js/main.js
+++ js/main.js
@@ -23,3 +23,10 @@ function changeText(obj, text) {
 
     obj.innerText = text;
 }
+
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("message","req","Please provide your message");
+frmvalidator.addValidation("email","email", "Please enter a valid email address");
+
