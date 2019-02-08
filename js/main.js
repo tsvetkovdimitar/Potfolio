7a55ac27feb6377696a3af6c79b9e50087505a95
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-02-08 03:13:19 +0000

    Some work on form validation. Not yet finished

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ window.onclick = function(event) {
             }
         }
     }
-}
+};
 
 function changeText(obj, text) {
 
@@ -27,6 +27,7 @@ function changeText(obj, text) {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("button_to_go_to_top_of_the_page").style.display = "block";
     } else {
@@ -39,3 +40,28 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+//Form Validation
+
+var inputName = document.getElementById('input-name');
+var inputEmail = document.getElementById('input-email');
+var inputMessage = document.getElementById('input-message');
+var button = document.querySelector('.send-button');
+var regEx = /\S+@\S+\.\S+/;
+var form = document.querySelector('.form');
+
+function validateForm(){
+
+    if (inputName.value.trim() !== "" && regEx.test(inputEmail.value) && inputMessage.value.trim() !== "") {
+        button.style.backgroundColor = '#0d0d0d';
+        button.disabled = false;
+    } else {
+        button.style.backgroundColor = '#404040';
+        button.disabled = true;
+    }
+}
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Submitted');
+});
